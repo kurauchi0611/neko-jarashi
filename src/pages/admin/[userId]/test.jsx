0d0dd8a93a3e08a75c00459ad1b9c7060f987eaa
roll1226 <tomo_yasu_4724@icloud.com>
2020-07-21 17:28:58 +0900

    [UPDATE]Admin/App.jsx

--- src/pages/admin/[userId]/test.jsx
+++ src/pages/admin/[userId]/test.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { useRouter } from "next/router";
+import App from "../../../components/Admin/App";
 
 export default function Test() {
   const router = useRouter();
   console.log(router.query.userId);
 
-  return <p>Test Page</p>;
+  return (
+    <App>
+      <p>Test Page</p>
+    </App>
+  );
 }
